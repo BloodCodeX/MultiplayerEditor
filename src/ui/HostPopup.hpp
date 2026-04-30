307a6d6a4e216831dc8646b4e60d7add299f2eac
BloodCodeX <volodymyr13k@gmail.com>
2026-04-30 12:54:05 +0300

    Update HostPopup.hpp

--- src/ui/HostPopup.hpp
+++ src/ui/HostPopup.hpp
@@ -1,16 +1,12 @@
 #pragma once
-
 #include <Geode/Geode.hpp>
+#include <Geode/ui/Popup.hpp>
 
 using namespace geode::prelude;
 
-class HostPopup : public Popup<>{
-    protected:
-        CCLabelBMFont* m_ipLabel;
-        CCMenuItemSpriteExtra* hostBtn;
-
-        bool setup() override;
-        void onStartHost(CCObject*);
-    public:
-        static HostPopup* create();
+class HostPopup : public geode::Popup<> {
+protected:
+    bool setup() override;
+public:
+    static HostPopup* create();
 };
